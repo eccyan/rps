5b9d347f5cbdbd8d86c981c5fac0e35aca142f62
eccyan <g00.eccyan@gmail.com>
2011-01-29 15:04:12 +0900

    create master dump

--- sql/update_card_attribute_relations.sql
+++
@@ -1,8 +0,0 @@
-USE game;
-
-REPLACE INTO `card_attribute_relations` VALUES (1, 2, 'critical', NULL, now(), now());
-REPLACE INTO `card_attribute_relations` VALUES (2, 3, 'critical', NULL, now(), now());
-REPLACE INTO `card_attribute_relations` VALUES (3, 1, 'critical', NULL, now(), now());
-REPLACE INTO `card_attribute_relations` VALUES (1, 3, 'ineffectual', NULL, now(), now());
-REPLACE INTO `card_attribute_relations` VALUES (2, 1, 'ineffectual', NULL, now(), now());
-REPLACE INTO `card_attribute_relations` VALUES (3, 2, 'ineffectual', NULL, now(), now());
